53c4a6070f89beaad75c24abcf15ea14a9c2f4e7
jaya <rohanfrd1234@gmail.com>
2026-06-11 13:56:51 +0530

    Add uncaught exception logging to VarahaApplication for device crash diagnostics

--- app/src/main/java/com/example/varahanest/VarahaApplication.kt
+++ app/src/main/java/com/example/varahanest/VarahaApplication.kt
@@ -7,6 +7,18 @@ class VarahaApplication : Application() {
     lateinit var container: AppContainer
 
     override fun onCreate() {
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            android.util.Log.e("VarahaCrash", "CRASH DETECTED", throwable)
+            try {
+                val file = java.io.File(externalCacheDir, "crash_log.txt")
+                file.writeText(android.util.Log.getStackTraceString(throwable))
+            } catch (e: Exception) {
+                // Ignore writing error
+            }
+            // Let the system handle the crash after logging
+            System.exit(1)
+        }
+
         super.onCreate()
         container = AppContainer(this)
     }
